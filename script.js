eed4f5f35821012caf29a24e1d37743da812999b
Krzychu <krzysztof.kerlin@op.pl>
2024-09-05 23:55:20 +0200

    Created gradient text view

---
+++ script.js
@@ -0,0 +1,25 @@
+const color1Input = document.getElementById('color1');
+const color2Input = document.getElementById('color2');
+const previewText = document.querySelector('.gradient-text');
+
+function updateGradient() {
+    const color1 = color1Input.value;
+    const color2 = color2Input.value;
+    const gradientCSS = `linear-gradient(${color1}, ${color2})`;
+
+    // Update gradient preview text
+    previewText.style.background = gradientCSS;
+    previewText.style.webkitBackgroundClip = 'text';
+    previewText.style.webkitTextFillColor = 'transparent';
+    previewText.style.backgroundClip = 'text';
+    previewText.style.color = 'transparent';
+
+}
+
+// Event listeners
+color1Input.addEventListener('input', updateGradient);
+color2Input.addEventListener('input', updateGradient);
+
+updateGradient();
+
+
