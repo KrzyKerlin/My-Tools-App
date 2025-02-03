dfacbd644e371be6ed0badf567797ecbffa1785e
KrzyKerlin <krzysztof.kerlin@op.pl>
2025-02-03 11:41:01 +0100

    Gradient css code does not expand in the container

--- src/views/gradientGenerator/gradientGenerator.vue
+++ src/views/gradientGenerator/gradientGenerator.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="gradient-container">
     <div class="preview">
-      <!-- Tekst z gradientem -->
-      <p ref="gradientText" class="gradient-text">GRADIENT TEXT</p>
+      <!-- text with gradient -->
+      <p ref="gradientText" class="gradient-text">GRADIENT</p>
     </div>
     <div class="controls">
       <div class="control-group">
-        <label for="color1">1:</label>
+        <label for="color1"></label>
         <input type="color" id="color1" v-model="color1" />
-        <label for="color2">2:</label>
+        <label for="color2"></label>
         <input type="color" id="color2" v-model="color2" />
       </div>
       <div class="control-group directions">
@@ -24,7 +24,7 @@
       </div>
     </div>
     <div class="gradient-code">
-      <!-- Podgląd wygenerowanego kodu CSS -->
+      <!-- generated CSS code -->
       <pre id="cssCode">{{ cssCode }}</pre>
       <button id="copyCode" @click="copyToClipboard">
         <i class="fas fa-copy"></i> Copy Code
